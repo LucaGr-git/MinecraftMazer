d02218cc2618521c9a6bb4578848fc88aab67a66
Luca grosso <S4093817@student.rmit.edu.au>
2024-10-14 17:40:01 +1100

    Added printArray method to Blockarray and added comments/refactored

--- BlockArray.cpp
+++ BlockArray.cpp
@@ -13,15 +13,15 @@ template <typename type> class BlockArray {
     int storageCap;
 
     // Current number of objects stored in the ArrayList
-    int currStored;
+    int logicalLen;
 
     public:
 
         // Default Constructor
         BlockArray() {
             arrPtr = new type[1];
-            storageCap = 2;
-            currStored = 0;
+            storageCap = 1;
+            logicalLen = 0;
         }
 
         // Destuctor to prevent memory leaks
@@ -29,16 +29,17 @@ template <typename type> class BlockArray {
             delete[] arrPtr; 
         }
 
-        void push_back(type newBlock) {
+        // method to place an element at the end of an array
+        void pushBack(type newBlock) {
             
             // If the storage cap is reached then a new array is created with more space and the old one is deleted
 
-            if (storageCap == currStored){
+            if (storageCap == logicalLen){
                 type* tempArrPtr = new type[2 * storageCap]; // Storage capacity is doubled
 
                 storageCap = 2 * storageCap;
                 // old elements are copied into the new array
-                for (int i = 0; i < currStored; ++i){
+                for (int i = 0; i < logicalLen; ++i){
                     tempArrPtr[i] = this->arrPtr[i];
                 }
 
@@ -49,15 +50,16 @@ template <typename type> class BlockArray {
             
             }
 
-            this->arrPtr[this->currStored++] = newBlock; // final element is updataded and currStored is iterated
+            this->arrPtr[this->logicalLen++] = newBlock; // final element is updataded and logical length is iterated
 
         }
 
+        // method to set a specific element of the list
         void set(int index, type newBlock){
             
-            // if index equals the capacity the push_back function is called
+            // if index equals the capacity the pushBack function is called
             if (index == this->storageCap){
-                push_back(newBlock);
+                pushBack(newBlock);
             }
             // otherwise the data at the index is replaced
             else{
@@ -65,24 +67,43 @@ template <typename type> class BlockArray {
             }
         }
 
-        
+
+        // method that returns the elelment at given index
         type get(int index)
         {
             // if index is in array
-            if (index < this->currStored)
+            if (index < this->logicalLen)
                 return this->arrPtr[index];
             // if index is not in array
-            return this->arrPtr[this->currStored - 1];
+            return this->arrPtr[this->logicalLen - 1];
         }
     
         
-        void pop_back() { 
-            this->currStored--; 
+        // method to remove final element of array
+        void popBack() { 
+            this->logicalLen--; 
         }
     
 
+        // method that returns logical length of array
         int getSize() { 
-            return this->currStored; 
+            return this->logicalLen; 
+        }
+
+        // method to print the array's elements and members
+        void printArray(const std::ostream &oss) {
+            // Print out array members
+            oss << "Printing BlockArray:\n";
+            oss << "    Current Maximum length:" << this->storageCap << "\n";
+            oss << "    Logical length:" << this->logicalLen << "\n";
+            oss << "Array:\n{";
+            // Print out each individual element in the array
+            for (int i = 0; i < this->logicalLen; ++i) {
+                // a comma or curly-bracket is added depending on if it is the last item in the list
+                oss << this->arrPtr[i] << ((i != this->logicalLen - 1)? ",":"}\n"); 
+            }
+
+
         }
 
 };
